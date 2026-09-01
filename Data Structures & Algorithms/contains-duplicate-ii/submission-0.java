8069d5079422bac9b311523a3dc3317625984407
Vimal Chaudhary <chaudharyvimal410@gmail.com>
2026-09-01 05:40:27 +0530

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+      
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j <Math.min(nums.length,i + k + 1); j++){
+                if(nums[i] == nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
