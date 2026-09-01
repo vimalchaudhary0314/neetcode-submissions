ad105084185a3dd2927fd9244f02101d69d09b31
Vimal Chaudhary <chaudharyvimal410@gmail.com>
2026-09-01 06:42:18 +0530

    Add: sliding-window-maximum - submission-3

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+       int n = nums.length;
+        int[] output = new int[n - k + 1];
+
+        for (int i = 0; i <= n - k; i++) {
+            int maxi = nums[i];
+            for (int j = i; j < i + k; j++) {
+                maxi = Math.max(maxi, nums[j]);
+            }
+            output[i] = maxi;
+        }
+
+        return output;
+    }
+}
